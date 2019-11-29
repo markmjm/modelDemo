5d96c197f7d98edeb3ab0aaa6e681e827fb84ead
markmjm <markmnei@gmail.com>
2019-11-29 10:30:38 -0500

    intial commit

---
+++ empApp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from empApp.models import Employee
+
+# Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=['firstName', 'lastName', 'salary', 'email']
+
+# admin.site.register(Employee)
+admin.site.register(Employee, EmployeeAdmin)
